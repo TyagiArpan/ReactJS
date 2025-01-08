b6b43cda5b76f61e5da8fc961f966a6d6df66a86
TyagiArpan <arpanmailtyagi007@gmail.com>
2025-01-08 22:12:21 +0530

    ReactRouter

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {id} = useParams()
+  return (
+    <h1 className='text-center'>User: {id}</h1>
+  )
+}
+
+export default User
